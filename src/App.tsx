418d8544107e1ce6b04d904b5f2d01de157db327
malikdemaximus <malikdemaximus@mail.ru>
2022-03-04 11:58:05 +0600

    first, last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import List from './components/List';
+import AddToList from './components/AddToList';
+
+export interface IState {
+  people: {
+    name: string
+    age: number
+    url: string
+    note?: string
+  }[]
+}
 
 function App() {
+  const [people, setPeople] = useState<IState["people"]>([
+    {
+      name: "LeBron James",
+      url: "https://www.gannett-cdn.com/presto/2020/01/26/USAT/80802abd-a62b-4420-8e15-ac4bd08c5929-2020-01-25_LeBron1.jpg?crop=2245,1688,x476,y589&width=2560",
+      age: 36,
+      note: 'Alleferic to staying on the same team'
+    },
+    {
+      name: "Kobe Briant",
+      url: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Kobe_Bryant_2014.jpg/1200px-Kobe_Bryant_2014.jpg",
+      age: 36
+    }
+  ])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People invited to my Party</h1>
+      <List people={people} />
+      <AddToList people={people} setPeople={setPeople} />
     </div>
   );
 }
---
+++ src/components/AddToList.tsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import { IState as Props } from '../App';
+
+interface IProps {
+  people: Props["people"],
+  setPeople: React.Dispatch<React.SetStateAction<Props["people"]>>
+}
+
+const AddToList: React.FC<IProps> = ({people, setPeople}) => {
+  const [input, setInput] = useState({
+    name: '',
+    age: '',
+    note: '',
+    url: ''
+  })
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleClick = (): void => {
+    if (
+      !input.name ||
+      !input.age ||
+      !input.url
+    ) {
+      return
+    }
+    setPeople([
+      ...people,
+      {
+        ...input,
+        age: parseInt(input.age)
+      }
+    ])
+
+    setInput({
+      name: '',
+      age: '',
+      note: '',
+      url: ''
+    })
+  }
+
+  return (
+    <div className="AddToList">
+      <input
+        type="text"
+        placeholder="Name"
+        className="AddToList-input"
+        value={input.name}
+        onChange={handleChange}
+        name="name"
+      />
+      <input
+        type="number"
+        placeholder="Age"
+        className="AddToList-input"
+        value={input.age}
+        onChange={handleChange}
+        name="age"
+      />
+      <input
+        type="text"
+        placeholder="Image url"
+        className="AddToList-input"
+        value={input.url}
+        onChange={handleChange}
+        name="url"
+      />
+      <textarea
+        placeholder="Note"
+        className="AddToList-input"
+        value={input.note}
+        onChange={handleChange}
+        name="note"
+      />
+      <button className="AddToList-btn" onClick={handleClick}>Add to List</button>
+    </div>
+  )
+}
+
+export default AddToList
